8d18b715deea1f87701d86a4a54645eb16637292
thecodingshrimp <leonard.stutzer@sap.com>
2026-04-22 15:43:48 +0200

    add rule resource
    Signed-off-by: thecodingshrimp <leonard.stutzer@sap.com>

--- provider/provider.go
+++ provider/provider.go
@@ -248,6 +248,7 @@ func Provider() *schema.Provider {
 			"opensearch_anomaly_detection":         resourceOpenSearchAnomalyDetection(),
 			"opensearch_sm_policy":                 resourceOpenSearchSMPolicy(),
 			"opensearch_correlation_rule":          resourceOpenSearchCorrelationRule(),
+			"opensearch_rule":                      resourceOpenSearchRule(),
 		},
 
 		DataSourcesMap: map[string]*schema.Resource{
